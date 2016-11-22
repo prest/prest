0394dd2c79cd1788ae706d5bcde146acfee1bc3a
Avelino <thiago@avelino.xxx>
2016-11-22 16:58:50 -0200

    main run negroni http up

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/caarlos0/env"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/jackc/pgx"
+
+	"github.com/nuveo/prest/config"
+)
+
+var conn *pgx.Conn
+
+func main() {
+	cfg := config.Prest{}
+	env.Parse(&cfg)
+
+	router := mux.NewRouter()
+	n := negroni.Classic()
+	n.UseHandler(router)
+	n.Run(fmt.Sprintf(":%v", cfg.HTTPPort))
+}
