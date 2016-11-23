79d32218b1fc8e5baa90a377f0e896c3d9f42581
Avelino <thiago@avelino.xxx>
2016-11-23 01:32:48 -0200

    used Prest struct for all config coming os env

--- config/config.go
+++ config/config.go
@@ -1,33 +1,11 @@
 package config
 
-import (
-	"os"
-
-	"github.com/jackc/pgx"
-)
-
 // Prest basic config
 type Prest struct {
 	// HTTPPort Declare which http port the PREST used
-	HTTPPort int `env:"PREST_HTTP_PORT" envDefault:"3000"`
-}
-
-// PrestPg PostgreSQL connection config
-func PrestPg() pgx.ConnConfig {
-	var config pgx.ConnConfig
-
-	config.Host = os.Getenv("PREST_HOST")
-	if config.Host == "" {
-		config.Host = "127.0.0.1"
-	}
-
-	config.User = os.Getenv("PREST_USER")
-	if config.User == "" {
-		config.User = os.Getenv("USER")
-	}
-
-	config.Password = os.Getenv("PREST_PASSWORD")
-	config.Database = os.Getenv("PREST_DATABASE")
-
-	return config
+	HTTPPort   int    `env:"PREST_HTTP_PORT" envDefault:"3000"`
+	PGHost     string `env:"PREST_PG_HOST", envDefault:"127.0.0.1"`
+	PGUser     string `env:"PREST_PG_USER"`
+	PGPass     string `env:"PREST_PG_PASS"`
+	PGDatabase string `env:"PREST_PG_DATABASE"`
 }
