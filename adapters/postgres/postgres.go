251df3b6010c23e0a18c3f1ae2d0382b6d5cd67d
Avelino <thiago@avelino.xxx>
2016-11-23 01:30:08 -0200

    migrate pgx to sqlx is more flexible to work with other data structure type (dynamic)

--- adapters/postgres/postgres.go
+++ adapters/postgres/postgres.go
@@ -1,17 +1,62 @@
 package postgres
 
 import (
+	"encoding/json"
 	"fmt"
 
-	"github.com/jackc/pgx"
+	"github.com/caarlos0/env"
+	"github.com/jmoiron/sqlx"
+	// Used pg drive on sqlx
+	_ "github.com/lib/pq"
+
 	"github.com/nuveo/prest/config"
 )
 
 // Conn connect on PostgreSQL
-func Conn() (conn *pgx.Conn) {
-	conn, err := pgx.Connect(config.PrestPg())
+// Used sqlx
+func Conn() (db *sqlx.DB) {
+	cfg := config.Prest{}
+	env.Parse(&cfg)
+
+	db, err := sqlx.Connect("postgres", fmt.Sprintf("user=%s dbname=%s sslmode=disable", cfg.PGUser, cfg.PGDatabase))
 	if err != nil {
 		panic(fmt.Sprintf("Unable to connection to database: %v\n", err))
 	}
+	return
+}
+
+// Query process queries
+func Query(SQL string) (jsonData []byte, err error) {
+	db := Conn()
+	rows, _ := db.Queryx(SQL)
+	defer rows.Close()
+
+	columns, _ := rows.Columns()
+
+	count := len(columns)
+	tableData := make([]map[string]interface{}, 0)
+	values := make([]interface{}, count)
+	valuePtrs := make([]interface{}, count)
+	for rows.Next() {
+		for i := 0; i < count; i++ {
+			valuePtrs[i] = &values[i]
+		}
+		rows.Scan(valuePtrs...)
+		entry := make(map[string]interface{})
+		for i, col := range columns {
+			var v interface{}
+			val := values[i]
+			b, ok := val.([]byte)
+			if ok {
+				v = string(b)
+			} else {
+				v = val
+			}
+			entry[col] = v
+		}
+		tableData = append(tableData, entry)
+	}
+	jsonData, err = json.Marshal(tableData)
+
 	return
 }
