452fc03b24cc73952565a176d502d945502a973b
Avelino <thiago@avelino.xxx>
2016-11-24 01:44:44 -0200

    used WhereByRequest in GetTables

--- controllers/tables.go
+++ controllers/tables.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -10,7 +11,18 @@ import (
 
 // GetTables list all (or filter) tables
 func GetTables(w http.ResponseWriter, r *http.Request) {
-	object, err := postgres.Query(statements.Tables)
+	requestWhere := postgres.WhereByRequest(r)
+	sqlTables := statements.Tables
+	if requestWhere != "" {
+		sqlTables = fmt.Sprint(
+			statements.TablesSelect,
+			statements.TablesWhere,
+			" AND ",
+			postgres.WhereByRequest(r),
+			statements.TablesOrderBy)
+	}
+
+	object, err := postgres.Query(sqlTables)
 	if err != nil {
 		log.Println(err)
 	}
