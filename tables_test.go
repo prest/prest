2709d4bd76839a3000524e93d1344cbab7d52e09
Felipe Oliveira <felipeweb.programador@gmail.com>
2016-11-29 14:47:39 -0200

    tables test

---
+++ tables_test.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"net/http"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestGetTables(t *testing.T) {
+	Convey("Get tables without custom where clause", t, func() {
+		r, err := http.NewRequest("GET", "/tables", nil)
+		So(err, ShouldBeNil)
+		validate(r)
+	})
+
+	Convey("Get tables with custom where clause", t, func() {
+		r, err := http.NewRequest("GET", "/tables?name=prest", nil)
+		So(err, ShouldBeNil)
+		validate(r)
+	})
+}
