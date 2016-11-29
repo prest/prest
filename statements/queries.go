de70df42e1ee76789aedec8e46e19fee46386f88
Felipe Oliveira <felipeweb.programador@gmail.com>
2016-11-29 11:38:20 -0200

    fix lint warnings

--- statements/queries.go
+++ statements/queries.go
@@ -2,30 +2,41 @@ package statements
 
 const (
 	// Databases list all data bases
+
+	// DatabasesSelect clause
 	DatabasesSelect = `
 SELECT
 	datname
 FROM
 	pg_database`
+	// DatabasesWhere clause
 	DatabasesWhere = `
 WHERE
 	NOT datistemplate`
+	// DatabasesOrderBy clause
 	DatabasesOrderBy = `
 ORDER BY
 	datname ASC`
+	// Databases default query
 	Databases = DatabasesSelect + DatabasesWhere + DatabasesOrderBy
 	// Schemas list all schema on data base
+
+	// SchemasSelect clause
 	SchemasSelect = `
 SELECT
 	schema_name
 FROM
 	information_schema.schemata`
+	// SchemasOrderBy clause
 	SchemasOrderBy = `
 ORDER BY
 	schema_name ASC`
+	// Schemas default query
 	Schemas = SchemasSelect + SchemasOrderBy
 
 	// Tables list all tables
+
+	// TablesSelect clause
 	TablesSelect = `
 SELECT
 	n.nspname as "schema",
@@ -44,13 +55,16 @@ FROM
 	pg_catalog.pg_class c
 LEFT JOIN
 	pg_catalog.pg_namespace n ON n.oid = c.relnamespace `
+	// TablesWhere clause
 	TablesWhere = `
 WHERE
 	c.relkind IN ('r','v','m','S','s','') AND
 	n.nspname !~ '^pg_toast' AND
 	n.nspname NOT IN ('information_schema', 'pg_catalog') AND
 	has_schema_privilege(n.nspname, 'USAGE') `
+	// TablesOrderBy clause
 	TablesOrderBy = `
 ORDER BY 1, 2`
+	// Tables default query
 	Tables = TablesSelect + TablesWhere + TablesOrderBy
 )
