7adf59783ae8decc1ace869311b5f608407bcc9e
Felipe Oliveira <felipeweb.programador@gmail.com>
2016-11-30 01:21:55 -0200

    try fix schema table tests

--- schemas_test.go
+++ schemas_test.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -10,13 +11,15 @@ import (
 func TestGetSchemas(t *testing.T) {
 	Convey("Get schemas without custom where clause", t, func() {
 		r, err := http.NewRequest("GET", "/schemas", nil)
+		w := httptest.NewRecorder()
 		So(err, ShouldBeNil)
-		validate(r, GetSchemas)
+		validate(w, r, GetSchemas)
 	})
 
 	Convey("Get schemas with custom where clause", t, func() {
 		r, err := http.NewRequest("GET", "/schemas?schema_name=public", nil)
+		w := httptest.NewRecorder()
 		So(err, ShouldBeNil)
-		validate(r, GetSchemas)
+		validate(w, r, GetSchemas)
 	})
 }
