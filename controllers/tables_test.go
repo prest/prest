effa38b7147d8491db8dcf51768ba2539145d3e1
Felipe Oliveira <felipeweb.programador@gmail.com>
2016-11-30 22:49:49 -0200

    select in tables test

--- controllers/tables_test.go
+++ controllers/tables_test.go
@@ -18,7 +18,7 @@ func TestGetTables(t *testing.T) {
 	})
 
 	Convey("Get tables with custom where clause", t, func() {
-		r, err := http.NewRequest("GET", "/tables?c.relname=prest", nil)
+		r, err := http.NewRequest("GET", "/tables?c.relname=test", nil)
 		w := httptest.NewRecorder()
 		So(err, ShouldBeNil)
 		validate(w, r, GetTables)
@@ -36,5 +36,19 @@ func TestGetTablesByDatabaseAndSchema(t *testing.T) {
 
 	Convey("Get tables by database and schema with custom where clause", t, func() {
 		doValidRequest(server.URL + "/prest/public?t.tablename=test")
+	})
+}
+
+func TestSelectFromTable(t *testing.T) {
+	router := mux.NewRouter()
+	router.HandleFunc("/{database}/{schema}/{table}", SelectFromTables).Methods("GET")
+	server := httptest.NewServer(router)
+	defer server.Close()
+	Convey("execute select in a table without custom where clause", t, func() {
+		doValidRequest(server.URL + "/prest/public")
+	})
+
+	Convey("execute select in a table with custom where clause", t, func() {
+		doValidRequest(server.URL + "/prest/public/test?name=nuveo")
 	})
 }
