940863b54ed474901ba84d2c9cdec7ad76982da2
Felipe Oliveira <felipeweb.programador@gmail.com>
2016-12-19 01:29:18 -0200

    update viper end fix tests

--- config_test.go
+++ config_test.go
@@ -10,22 +10,15 @@ import (
 
 func TestParse(t *testing.T) {
 	Convey("Verify if get default value", t, func() {
+		os.Setenv("PREST_CONF", "../prest.json")
 		viperCfg()
 		cfg := &Prest{}
 		err := Parse(cfg)
 		So(err, ShouldBeNil)
 		So(cfg.HTTPPort, ShouldEqual, 3000)
 	})
-	Convey("Verify if get env", t, func() {
-		os.Setenv("PREST_HTTP_PORT", "4000")
-		viperCfg()
-		cfg := &Prest{}
-		err := Parse(cfg)
-		So(err, ShouldBeNil)
-		So(cfg.HTTPPort, ShouldEqual, 4000)
-	})
 	Convey("Verify if get toml", t, func() {
-		os.Setenv("PREST_CONF", "testdata/prest.toml")
+		os.Setenv("PREST_CONF", "../testdata/prest.json")
 		viperCfg()
 		cfg := &Prest{}
 		err := Parse(cfg)
@@ -33,9 +26,18 @@ func TestParse(t *testing.T) {
 		So(cfg.HTTPPort, ShouldEqual, 6000)
 		So(cfg.PGDatabase, ShouldEqual, "prest")
 	})
+	Convey("Verify if get env", t, func() {
+		os.Setenv("PREST_CONF", "../prest.json")
+		os.Setenv("PREST_HTTP_PORT", "4000")
+		viperCfg()
+		cfg := &Prest{}
+		err := Parse(cfg)
+		So(err, ShouldBeNil)
+		So(cfg.HTTPPort, ShouldEqual, 4000)
+	})
 	Convey("Verify if env override toml", t, func() {
 		os.Setenv("PREST_HTTP_PORT", "4000")
-		os.Setenv("PREST_CONF", "testdata/prest.toml")
+		os.Setenv("PREST_CONF", "../testdata/prest.json")
 		viperCfg()
 		cfg := &Prest{}
 		err := Parse(cfg)
