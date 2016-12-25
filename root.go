42327e88ad9f5e3e3404d9abe3ff6a85acd0e938
Felipe Oliveira <felipeweb.programador@gmail.com>
2016-12-25 20:19:00 -0200

    remove cobra override default config code

--- root.go
+++ root.go
@@ -13,7 +13,6 @@ import (
 	"github.com/nuveo/prest/config"
 	"github.com/nuveo/prest/controllers"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 	"github.com/urfave/negroni"
 )
 
@@ -42,33 +41,6 @@ func Execute() {
 func init() {
 	prestConfig = config.Prest{}
 	config.Parse(&prestConfig)
-	// prest
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports Persistent Flags, which, if defined here,
-	// will be global for your application.
-
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.prest.yaml)")
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" { // enable ability to specify config file via flag
-		viper.SetConfigFile(cfgFile)
-	}
-
-	viper.SetConfigName(".prest") // name of config file (without extension)
-	viper.AddConfigPath("$HOME")  // adding home directory as first search path
-	viper.AutomaticEnv()          // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
 
 func app() {
