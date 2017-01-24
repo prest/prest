8c156f15979d3b373568f26c1b3aedc31838a4cf
Mario Idival <marioidival@gmail.com>
2017-01-24 18:21:40 -0300

    [WIP] Change Access Control verification (#80)
    * Create utils to middleware
    
    * Remove old permission verifications
    
    * Create middleware to AccessControl
    
    * Remove old permission verification in tables.go
    
    * Create package middleware
    
    * Change name of middlewares utils file

---
+++ utils.go
@@ -0,0 +1,38 @@
+package middlewares
+
+import (
+	"strings"
+
+	"github.com/nuveo/prest/statements"
+)
+
+func getVars(path string) (paths map[string]string) {
+	pathList := strings.Split(path, "/")
+
+	if len(pathList) < 3 || len(pathList) > 4 {
+		return nil
+	} else if len(pathList) == 4 {
+		pathList = pathList[1:]
+	}
+	paths = make(map[string]string, 0)
+	paths["database"] = pathList[0]
+	paths["schema"] = pathList[1]
+	paths["table"] = pathList[2]
+
+	return
+}
+
+func permissionByMethod(method string) (permission string) {
+	switch method {
+	case "GET":
+		permission = statements.READ
+	case "POST", "PATCH", "PUT":
+		permission = statements.WRITE
+	case "DELETE":
+		permission = statements.DELETE
+	default:
+		permission = ""
+	}
+
+	return
+}
