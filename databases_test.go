2bcdab25dac772b1c36bbb0e13cf4eb302140661
Mario Idival <marioidival@gmail.com>
2017-02-01 16:46:47 -0300

    Improvements testing package (#93)
    * Fixed PREST_CONF env
    
    * Update vendor package
    
    * Change testing package on adapters/postgres
    
    * Change testing package on controllers
    
    * Change testing package on config

--- databases_test.go
+++ databases_test.go
@@ -1,69 +1,39 @@
 package controllers
 
 import (
-	"net/http"
 	"testing"
 
 	"net/http/httptest"
 
 	"github.com/gorilla/mux"
 	"github.com/nuveo/prest/api"
-	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestGetDatabases(t *testing.T) {
-	Convey("Get databases without custom where clause", t, func() {
-		r, err := http.NewRequest("GET", "/databases", nil)
-		w := httptest.NewRecorder()
-		So(err, ShouldBeNil)
-		validate(w, r, GetDatabases, "TestGetDatabases")
-	})
+	var testCases = []struct {
+		description string
+		url         string
+		method      string
+		status      int
+	}{
+		{"Get databases without custom where clause", "/databases", "GET", 200},
+		{"Get databases with custom where clause", "/databases?datname=$eq.prest", "GET", 200},
+		{"Get databases with custom order clause", "/databases?_order=datname", "GET", 200},
+		{"Get databases with custom where clause and pagination", "/databases?datname=$eq.prest&_page=1&_page_size=20", "GET", 200},
+		{"Get databases with COUNT clause", "/databases?_count=", "GET", 200},
+		{"Get databases with custom where invalid clause", "/databases?0datname=prest", "GET", 400},
+		{"Get databases with custom where and pagination invalid", "/databases?datname=$eq.prest&_page=A", "GET", 400},
+		{"Get databases with noexistent column", "/databases?datatata=$eq.test", "GET", 500},
+	}
 
-	Convey("Get databases with custom where clause", t, func() {
-		r, err := http.NewRequest("GET", "/databases?datname=$eq.prest", nil)
-		w := httptest.NewRecorder()
-		So(err, ShouldBeNil)
-		validate(w, r, GetDatabases, "TestGetDatabases")
-	})
+	r := api.Request{}
+	router := mux.NewRouter()
+	router.HandleFunc("/databases", GetDatabases).Methods("GET")
+	server := httptest.NewServer(router)
+	defer server.Close()
 
-	Convey("Get databases with custom order clause", t, func() {
-		r, err := http.NewRequest("GET", "/databases?_order=datname", nil)
-		w := httptest.NewRecorder()
-		So(err, ShouldBeNil)
-		validate(w, r, GetDatabases, "TestGetDatabases")
-	})
-
-	Convey("Get databases with custom where invalid clause", t, func() {
-		router := mux.NewRouter()
-		router.HandleFunc("/databases", GetDatabases).Methods("GET")
-		server := httptest.NewServer(router)
-		defer server.Close()
-
-		r := api.Request{}
-		doRequest(server.URL+"/databases?0datname=prest", r, "GET", 400, "GetDatabases")
-	})
-
-	Convey("Get databases with custom where and pagination invalid", t, func() {
-		router := mux.NewRouter()
-		router.HandleFunc("/databases", GetDatabases).Methods("GET")
-		server := httptest.NewServer(router)
-		defer server.Close()
-
-		r := api.Request{}
-		doRequest(server.URL+"/databases?datname=$eq.prest&_page=A", r, "GET", 400, "GetDatabases")
-	})
-
-	Convey("Get databases with custom where clause and pagination", t, func() {
-		r, err := http.NewRequest("GET", "/databases?datname=$eq.prest&_page=1&_page_size=20", nil)
-		w := httptest.NewRecorder()
-		So(err, ShouldBeNil)
-		validate(w, r, GetDatabases, "TestGetDatabases")
-	})
-
-	Convey("Get databases with COUNT clause", t, func() {
-		r, err := http.NewRequest("GET", "/databases?_count=", nil)
-		w := httptest.NewRecorder()
-		So(err, ShouldBeNil)
-		validate(w, r, GetDatabases, "TestGetDatabases")
-	})
+	for _, tc := range testCases {
+		t.Log(tc.description)
+		doRequest(t, server.URL+tc.url, r, tc.method, tc.status, "GetDatabases")
+	}
 }
