72336f149fb203bf3a41c7f1addcd570f145b68c
Felipe Oliveira <felipeweb.programador@gmail.com>
2017-02-23 10:03:41 -0300

    add test case

--- controllers/schemas_test.go
+++ controllers/schemas_test.go
@@ -15,15 +15,16 @@ func TestGetSchemas(t *testing.T) {
 		url         string
 		method      string
 		status      int
+		body        string
 	}{
-		{"Get schemas without custom where clause", "/schemas", "GET", 200},
-		{"Get schemas with custom where clause", "/schemas?schema_name=$eq.public", "GET", 200},
-		{"Get schemas with custom order clause", "/schemas?schema_name=$eq.public&_order=schema_name", "GET", 200},
-		{"Get schemas with custom where clause and pagination", "/schemas?schema_name=$eq.public&_page=1&_page_size=20", "GET", 200},
-		{"Get schemas with COUNT clause", "/schemas?_count=*", "GET", 200},
-		{"Get schemas with custom where invalid clause", "/schemas?0schema_name=$eq.public", "GET", 400},
-		{"Get schemas with custom where and pagination invalid", "/schemas?schema_name=$eq.public&_page=A", "GET", 400},
-		{"Get schemas with noexistent column", "/schemas?schematame=$eq.test", "GET", 500},
+		{"Get schemas without custom where clause", "/schemas", "GET", 200, ""},
+		{"Get schemas with custom where clause", "/schemas?schema_name=$eq.public", "GET", 200, ""},
+		{"Get schemas with custom order clause", "/schemas?schema_name=$eq.public&_order=schema_name", "GET", 200, ""},
+		{"Get schemas with custom where clause and pagination", "/schemas?schema_name=$eq.public&_page=1&_page_size=20", "GET", 200, ""},
+		{"Get schemas with COUNT clause", "/schemas?_count=*", "GET", 200, ""},
+		{"Get schemas with custom where invalid clause", "/schemas?0schema_name=$eq.public", "GET", 400, ""},
+		{"Get schemas with custom where and pagination invalid", "/schemas?schema_name=$eq.public&_page=A", "GET", 400, ""},
+		{"Get schemas with noexistent column", "/schemas?schematame=$eq.test", "GET", 500, ""},
 	}
 
 	router := mux.NewRouter()
@@ -34,6 +35,7 @@ func TestGetSchemas(t *testing.T) {
 	r := api.Request{}
 	for _, tc := range testCases {
 		t.Log(tc.description)
-		doRequest(t, server.URL+tc.url, r, tc.method, tc.status, "GetSchemas")
+		doRequest(t, server.URL+tc.url, r, tc.method, tc.status, "GetSchemas", tc.body)
+
 	}
 }
