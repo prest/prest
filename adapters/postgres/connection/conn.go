ced2b129a7da2855c67837d4723d2ce19a48a1d4
Henrique Chehad <hchehad@gmail.com>
2017-03-16 18:01:15 -0300

    created connection timeout configuration

--- adapters/postgres/connection/conn.go
+++ adapters/postgres/connection/conn.go
@@ -22,7 +22,7 @@ func MustGet() *sqlx.DB {
 	if db == nil {
 		cfg = config.Prest{}
 		config.Parse(&cfg)
-		dbURI := fmt.Sprintf("user=%s dbname=%s host=%s port=%v sslmode=disable", cfg.PGUser, cfg.PGDatabase, cfg.PGHost, cfg.PGPort)
+		dbURI := fmt.Sprintf("user=%s dbname=%s host=%s port=%v sslmode=disable connect_timeout=%d", cfg.PGUser, cfg.PGDatabase, cfg.PGHost, cfg.PGPort, cfg.PGConnTimeout)
 		if cfg.PGPass != "" {
 			dbURI += " password=" + cfg.PGPass
 		}
