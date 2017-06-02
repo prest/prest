9ba5eb68a75d17bbe32e395efec1c54df19da1b0
Cesar Gimenes <crg@crg.eti.br>
2017-06-02 10:15:39 -0300

    remove statement_timeout test

--- adapters/postgres/postgres_test.go
+++ adapters/postgres/postgres_test.go
@@ -794,27 +794,6 @@ func TestColumnsByRequest(t *testing.T) {
 	}
 }
 
-func TestTimeout(t *testing.T) {
-	_, err := Query("SET statement_timeout TO 10")
-	if err != nil {
-		t.Errorf("Error setting statement_timeout: %s", err)
-	}
-
-	_, err = Query("SELECT pg_sleep(1000)")
-	if err == nil {
-		t.Errorf("Error should not be nil")
-	}
-
-	if !strings.Contains(err.Error(), "statement timeout") {
-		t.Errorf("Returned different error: %s", err)
-	}
-
-	_, err = Query("SET statement_timeout TO 0")
-	if err != nil {
-		t.Errorf("Error disabling statement_timeout")
-	}
-}
-
 func TestMockUsage(t *testing.T) {
 	mock, err := connection.UseMockDB("postgres")
 	if err != nil {
