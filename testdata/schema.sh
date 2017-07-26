56091502c883d1fed5cde448d407590cb5fb75ac
Antônio Roberto <forevertonny@gmail.com>
2017-07-25 23:48:13 -0300

    fix typo

--- testdata/schema.sh
+++ testdata/schema.sh
@@ -3,8 +3,8 @@ DB_USER=${PREST_PG_USER:-postgres}
 DB_PORT=${PREST_PG_PORT:-5438}
 DB_NAME=${PREST_PG_DATABASE:-prest} 
 
-psql -h $DH_HOST -p $DB_PORT -U $DB_USER -c "DROP DATABASE IF EXISTS $DB_NAME"
-psql -h $DH_HOST -p $DB_PORT -U $DB_USER -c "create database $DB_NAME;"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "DROP DATABASE IF EXISTS $DB_NAME"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "create database $DB_NAME;"
 
 # Create tables
 psql -d $DB_NAME -h $DB_HOST -p $DB_PORT -U $DB_USER -c "create table test(id serial, name text);"
