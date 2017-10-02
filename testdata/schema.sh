87b211bffef7b89fc6dc9dd774e154a25c1c9ebe
Ivan Voronchihin <bege13mot@gmail.com>
2017-10-02 23:52:30 +0400

    Update schema.sh

--- testdata/schema.sh
+++ testdata/schema.sh
@@ -3,4 +3,4 @@ DB_USER=${PREST_PG_USER:-postgres}
 DB_PORT=${PREST_PG_PORT:-5438}
 DB_NAME=${PREST_PG_DATABASE:-prest} 
 
-psql -h $DB_HOST -p $DB_PORT -U $DB_USER -v DB_NAME=$DB_NAME -f schema.sql
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER -v DB_NAME=$DB_NAME -f testdata/schema.sql
