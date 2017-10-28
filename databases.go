0d568c2474bb92d47ae28896b265945207945f10
Cesar Gimenes <crg@crg.eti.br>
2017-10-28 12:44:32 -0200

    Joel`s conflicts and merge manuali (#11)
    Joel`s conflicts and merge manualy

--- databases.go
+++ databases.go
@@ -5,26 +5,26 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/prest/adapters/postgres"
+	"github.com/prest/config"
 	"github.com/prest/statements"
 )
 
 // GetDatabases list all (or filter) databases
 func GetDatabases(w http.ResponseWriter, r *http.Request) {
-	requestWhere, values, err := postgres.WhereByRequest(r, 1)
+	requestWhere, values, err := config.PrestConf.Adapter.WhereByRequest(r, 1)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	query, hasCount := postgres.DatabaseClause(r)
+	query, hasCount := config.PrestConf.Adapter.DatabaseClause(r)
 	sqlDatabases := fmt.Sprint(query, statements.DatabasesWhere)
 
 	if requestWhere != "" {
 		sqlDatabases = fmt.Sprint(sqlDatabases, " AND ", requestWhere)
 	}
 
-	distinct, err := postgres.DistinctClause(r)
+	distinct, err := config.PrestConf.Adapter.DistinctClause(r)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
@@ -33,7 +33,8 @@ func GetDatabases(w http.ResponseWriter, r *http.Request) {
 		sqlDatabases = strings.Replace(sqlDatabases, "SELECT", distinct, 1)
 	}
 
-	order, err := postgres.OrderByRequest(r)
+	order, err := config.PrestConf.Adapter.OrderByRequest(r)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
@@ -45,14 +46,14 @@ func GetDatabases(w http.ResponseWriter, r *http.Request) {
 		sqlDatabases = fmt.Sprint(sqlDatabases, fmt.Sprintf(statements.DatabasesOrderBy, statements.FieldDatabaseName))
 	}
 
-	page, err := postgres.PaginateIfPossible(r)
+	page, err := config.PrestConf.Adapter.PaginateIfPossible(r)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	sqlDatabases = fmt.Sprint(sqlDatabases, " ", page)
-	sc := postgres.Query(sqlDatabases, values...)
+	sc := config.PrestConf.Adapter.Query(sqlDatabases, values...)
 	if sc.Err() != nil {
 		http.Error(w, sc.Err().Error(), http.StatusBadRequest)
 		return
