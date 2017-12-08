e7a564d932767642c6be214e73405e5f0ba9a3a3
Tiago Temporin <tiaguinhocab@gmail.com>
2017-12-08 23:07:30 +0000

    add allowCredentials header to every request

--- middlewares.go
+++ middlewares.go
@@ -65,10 +65,10 @@ func JwtMiddleware(key string) negroni.Handler {
 func Cors(origin []string, headers []string) negroni.Handler {
 	return negroni.HandlerFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 		w.Header().Set(headerAllowOrigin, strings.Join(origin, ","))
+		w.Header().Set(headerAllowCredentials, strconv.FormatBool(true))
 		if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
 			w.Header().Set(headerAllowMethods, strings.Join(defaultAllowMethods, ","))
 			w.Header().Set(headerAllowHeaders, strings.Join(headers, ","))
-			w.Header().Set(headerAllowCredentials, strconv.FormatBool(true))
 			if allowed := checkCors(r, origin); !allowed {
 				w.WriteHeader(http.StatusForbidden)
 				return
