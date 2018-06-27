07dcf603c9a1dd84cd20c0425e5354a329c4c624
felipeweb <felipeweb.programador@gmail.com>
2018-06-26 21:28:42 -0300

    change way to show version so goreleaser can understand

--- prest.go
+++ prest.go
@@ -1,23 +1,13 @@
 package helpers
 
-import (
-	"fmt"
-)
-
-const (
-	// PrestVersionNumber Major and minor version.
-	PrestVersionNumber = 0.3
-
-	// PrestPatchVersion Increment this for bug releases
-	PrestPatchVersion = 0
-)
-
 var (
+	// PrestVersionNumber repesemts prest version.
+	PrestVersionNumber = "0.3.0"
 	// CommitHash for version
 	CommitHash string
 )
 
 // PrestReleaseVersion is same as pREST Version.
 func PrestReleaseVersion() string {
-	return fmt.Sprintf("%.2g.%d", PrestVersionNumber, PrestPatchVersion)
+	return PrestVersionNumber
 }
