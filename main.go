8d829b582ab38b7bd770df613368651e700437bb
felipeweb <felipeweb.programador@gmail.com>
2018-07-09 19:59:46 -0300

    postgres.Load ins't mandatory for all commands
    migrate and version commands can be ran without driver load

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"github.com/prest/adapters/postgres"
 	"github.com/prest/cmd"
 	"github.com/prest/config"
 )
 
 func main() {
 	config.Load()
-	postgres.Load()
 	cmd.Execute()
 }
