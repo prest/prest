0f03183b3b98c74d8ddfb3678178b9b4cfeea5c3
Tiago Temporin <tiaguinhocab@gmail.com>
2020-07-23 16:11:08 -0300

    chore: remove GO111MODULE

--- testdata/migrations_test.sh
+++ testdata/migrations_test.sh
@@ -5,4 +5,4 @@ DB_USER=${PREST_PG_USER:-postgres}
 DB_PORT=${PREST_PG_PORT:-5438}
 DB_NAME=${PREST_PG_DATABASE:-prest} 
 DB_URL="postgres://${DB_USER}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable"
-env GO111MODULE=on go run main.go migrate  --url="$DB_URL" up
+env go run main.go migrate  --url="$DB_URL" up
