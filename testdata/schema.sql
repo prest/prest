be80132c2471d759cc63dd03e0493f127ba27df1
Tiago Temporin <tiaguinhocab@gmail.com>
2020-07-28 16:09:48 -0300

    chore: add missing table

--- testdata/schema.sql
+++ testdata/schema.sql
@@ -1,5 +1,6 @@
 -- CREATE TABLES
 CREATE TABLE test(id serial, name text);
+CREATE TABLE "Reply"(id serial, name text);
 CREATE TABLE test2(name text, number integer);
 CREATE TABLE test3(id serial, name text UNIQUE);
 CREATE TABLE test4(id serial primary key, name text UNIQUE);
@@ -19,6 +20,7 @@ CREATE TABLE test_group_by_table(id serial, name text, age integer, salary int);
 
 -- Inserts
 INSERT INTO test (name) VALUES ('prest tester');
+INSERT INTO "Reply" (name) VALUES ('prest tester');
 INSERT INTO test (name) VALUES ('tester02');
 INSERT INTO test2 (name, number) VALUES ('tester02', 2);
 INSERT INTO test3 (name) VALUES ('prest');
@@ -29,14 +31,14 @@ INSERT INTO test_readonly_access (name) VALUES ('test01');
 INSERT INTO test_write_and_delete_access (name) VALUES ('test01');
 INSERT INTO test_list_only_id (name) VALUES ('test01');
 INSERT INTO test_deleteonly_access (name) VALUES ('test01');
-INSERT INTO test_jsonb_bug (name, data) values('goku', '{"techniques": ["kamehameha", "kaioken", "genki-dama"]}');
-INSERT INTO test_jsonb_bug (name, data) values('goku', '{"description": "testing"}');
-INSERT INTO testarray (id, data) values(100, '{"Gohan","Goten"}');
+INSERT INTO test_jsonb_bug (name, data) VALUES('goku', '{"techniques": ["kamehameha", "kaioken", "genki-dama"]}');
+INSERT INTO test_jsonb_bug (name, data) VALUES('goku', '{"description": "testing"}');
+INSERT INTO testarray (id, data) VALUES(100, '{"Gohan","Goten"}');
 
-INSERT INTO test_group_by_table(name, age, salary) values('joao', 20, 1250);
-INSERT INTO test_group_by_table(name, age, salary) values('maria', 19, 3999);
-INSERT INTO test_group_by_table(name, age, salary) values('gopher', 20, 100);
-INSERT INTO test_group_by_table(name, age, salary) values('guitarra humana', 19, 3998);
+INSERT INTO test_group_by_table(name, age, salary) VALUES('joao', 20, 1250);
+INSERT INTO test_group_by_table(name, age, salary) VALUES('maria', 19, 3999);
+INSERT INTO test_group_by_table(name, age, salary) VALUES('gopher', 20, 100);
+INSERT INTO test_group_by_table(name, age, salary) VALUES('guitarra humana', 19, 3998);
 
 -- Views
 CREATE TABLE table_to_view(id serial, name text, celphone text);
