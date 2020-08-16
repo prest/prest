f9bad3d8cbc10e357a8b863a5d93caad17d4bb99
Avelino <t@avelino.xxx>
2020-08-16 16:30:35 -0300

    deploy via sh script
    Signed-off-by: Avelino <t@avelino.xxx>

---
+++ scripts/releaser-tag.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+docker build . -t prest/prest:v1 && \
+docker push prest/prest:v1 && \
+docker tag prest/prest:v1 prest/prest:$TRAVIS_TAG && \
+docker push prest/prest:$TRAVIS_TAG && \
+curl -sL https://git.io/goreleaser | bash
