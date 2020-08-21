d95cc519c0d2d2f6ab0fc09ce8fe7a4b9ebb888c
Avelino <t@avelino.xxx>
2020-08-21 17:39:29 -0300

    remove docbuild folder if exist on netlify deploy
    Signed-off-by: Avelino <t@avelino.xxx>

--- scripts/documation-build.sh
+++ scripts/documation-build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
+rm -rf docbuild && \
 git clone https://github.com/prest/prest.github.io docbuild && \
-    rm -rf docbuild/content && \
-    cp -rf docs docbuild/content && \
-    cd docbuild && \
-    hugo $@
+rm -rf docbuild/content && \
+cp -rf docs docbuild/content && \
+cd docbuild && \
+hugo $@
