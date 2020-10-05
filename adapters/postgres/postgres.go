e49d7451d2d5dece82653d171b1d76ad0f01b1cf
Sarath <sveedu@akamai.com>
2020-10-05 12:47:57 +0530

    Fix insertTableNameRegex and insertTableNameQuotesRegex to support hyphen in dbname
    Signed-off-by: Sarath <sveedu@akamai.com>

--- adapters/postgres/postgres.go
+++ adapters/postgres/postgres.go
@@ -97,8 +97,8 @@ func Load() {
 
 func init() {
 	removeOperatorRegex = regexp.MustCompile(`\$[a-z]+.`)
-	insertTableNameRegex = regexp.MustCompile(`(?i)INTO\s+([\w|\.]*\.)*(\w+)\s*\(`)
-	insertTableNameQuotesRegex = regexp.MustCompile(`(?i)INTO\s+([\w|\.|"]*\.)*"(\w+)"\s*\(`)
+	insertTableNameRegex = regexp.MustCompile(`(?i)INTO\s+([\w|\.|-]*\.)*([\w|-]+)\s*\(`)
+	insertTableNameQuotesRegex = regexp.MustCompile(`(?i)INTO\s+([\w|\.|"|-]*\.)*"([\w|-]+)"\s*\(`)
 	groupRegex = regexp.MustCompile(`\"(.+?)\"`)
 }
 
