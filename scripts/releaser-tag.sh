9fcc17393dc4fe8b53821644f3423a8f8f87a772
Avelino <t@avelino.xxx>
2020-10-27 14:08:28 -0300

    remove docker login on travis (#463)
    remove docker login when not generating a release
    ```
    Error response from daemon: Get https://registry-1.docker.io/v2/: unauthorized: incorrect username or password
    The command "docker login -u="$DOCKER_LOGIN" -p="$DOCKER_PASSWORD"" failed and exited with 1 during .
    ```
    
    Signed-off-by: Avelino <t@avelino.xxx>

--- scripts/releaser-tag.sh
+++ scripts/releaser-tag.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 docker build . -t prest/prest:v1 && \
+docker login -u="$DOCKER_LOGIN" -p="$DOCKER_PASSWORD" && \
 docker push prest/prest:v1 && \
 docker tag prest/prest:v1 prest/prest:$TRAVIS_TAG && \
 docker push prest/prest:$TRAVIS_TAG && \
