81f7473af35c92e707f91a5a08d56eca3ef75177
Tiago Temporin <tiaguinhocab@gmail.com>
2020-10-30 16:41:36 -0300

    feat(cmd): implement auth (up/down) migrations

--- cmd/root.go
+++ cmd/root.go
@@ -34,6 +34,8 @@ var RootCmd = &cobra.Command{
 // Execute adds all child commands to the root command sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	upCmd.AddCommand(authUpCmd)
+	downCmd.AddCommand(authDownCmd)
 	migrateCmd.AddCommand(downCmd)
 	migrateCmd.AddCommand(mversionCmd)
 	migrateCmd.AddCommand(nextCmd)
