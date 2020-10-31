b6a83b7130c81d3bd6b3b2dcd00bcf199a30e9c4
Avelino <t@avelino.xxx>
2020-10-31 17:04:40 -0300

    run migrate up auth on tests
    Signed-off-by: Avelino <t@avelino.xxx>

--- testdata/migrations_test.sh
+++ testdata/migrations_test.sh
@@ -4,6 +4,7 @@ export PREST_MIGRATIONS=./testdata/migrations
 DB_HOST=${PREST_PG_HOST:-localhost}
 DB_USER=${PREST_PG_USER:-postgres}
 DB_PORT=${PREST_PG_PORT:-5438}
-DB_NAME=${PREST_PG_DATABASE:-prest} 
+DB_NAME=${PREST_PG_DATABASE:-prest}
 DB_URL="postgres://${DB_USER}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable"
-env go run ./cmd/prestd/main.go migrate --url="$DB_URL" up
+env go run ./cmd/prestd/main.go migrate --url="$DB_URL" up && \
+    go run ./cmd/prestd/main.go migrate --url="$DB_URL" up auth
