38429627d9557385dab8aa8c2de8e71e2f880e2c
Avelino <t@avelino.xxx>
2021-01-14 19:41:24 -0300

    force git checkout on goreleaser by tag
    Signed-off-by: Avelino <t@avelino.xxx>

--- scripts/releaser-tag.sh
+++ scripts/releaser-tag.sh
@@ -4,4 +4,5 @@ docker login -u="$DOCKER_LOGIN" -p="$DOCKER_PASSWORD" && \
 docker push prest/prest:v1 && \
 docker tag prest/prest:v1 prest/prest:$TRAVIS_TAG && \
 docker push prest/prest:$TRAVIS_TAG && \
+git checkout . && \
 curl -sL https://git.io/goreleaser | bash
