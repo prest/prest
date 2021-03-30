1e77b7e82b96d496c615d74f4f978f4dc6abda7a
Avelino <t@avelino.xxx>
2021-03-30 17:25:00 -0300

    generate pREST version 1.0.8
    Signed-off-by: Avelino <t@avelino.xxx>

--- helpers/prest.go
+++ helpers/prest.go
@@ -2,7 +2,7 @@ package helpers
 
 var (
 	// PrestVersionNumber repesemts prest version.
-	PrestVersionNumber = "1.0.7"
+	PrestVersionNumber = "1.0.8"
 	// CommitHash for version
 	CommitHash string
 )
