6dfc7b256079fd146d5bf14086744a6b91048bf7
Avelino <t@avelino.xxx>
2021-03-30 09:49:50 -0300

    declare prefix in messages generated at the entrypoint assist in problem identification
    Signed-off-by: Avelino <t@avelino.xxx>

--- etc/entrypoint.sh
+++ etc/entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-echo "Waiting for port $PREST_PG_HOST:$PREST_PG_PORT to become available..."
+echo "[prestd] Waiting for port $PREST_PG_HOST:$PREST_PG_PORT to become available..."
 while "! nc -z $PREST_PG_HOST $PREST_PG_PORT" 2>/dev/null
 do
     ((elapsed=elapsed+1))
     if [ "$elapsed" -gt 90 ]
     then
-        echo "TIMED OUT !"
+        echo "[prestd] TIMED OUT!"
         exit 1
     fi
     sleep 1;
 done
 
 sleep 5;
-echo "Ready hosting $PREST_PG_HOST to port $PREST_PG_PORT !"
-/app/prestd $@
+echo "[prestd] Ready hosting $PREST_PG_HOST to port $PREST_PG_PORT !"
+/bin/prestd $@
