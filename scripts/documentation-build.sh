6e12aab3abf10e08bb19b46196127471984764ea
Avelino <t@avelino.xxx>
2021-04-21 09:31:41 -0300

    update docker build script assets support
    Signed-off-by: Avelino <t@avelino.xxx>

--- scripts/documentation-build.sh
+++ scripts/documentation-build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-rm -rf docbuild && \
-git clone https://github.com/prest/doc-template docbuild && \
-rm -rf docbuild/content && \
-cp -rf docs docbuild/content && \
-cd docbuild && \
-hugo $@
+rm -rf docbuild &&
+    git clone https://github.com/prest/doc-template docbuild &&
+    rm -rf docbuild/content &&
+    cp -rf docs docbuild/content &&
+    mv docbuild/content/assets docbuild/static/content &&
+    cd docbuild &&
+    hugo $@
