178becd15ecc570345845e2d806a4a14e59c4966
Fabrízio de Royes Mello <fabriziomello@gmail.com>
2021-04-28 17:25:22 -0300

    Fix shell script that lacks of '\' at the end of each line of the command line splitted into several lines.
    Signed-off-by: Fabrízio de Royes Mello <fabriziomello@gmail.com>

--- scripts/documentation-build.sh
+++ scripts/documentation-build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-rm -rf docbuild &&
-    git clone https://github.com/prest/doc-template docbuild &&
-    rm -rf docbuild/content &&
-    cp -rf docs docbuild/content &&
-    mv docbuild/content/assets docbuild/static/content &&
-    cd docbuild &&
-    hugo $@
+
+rm -rf docbuild && \
+    git clone https://github.com/prest/doc-template docbuild && \
+    rm -rf docbuild/content && \
+    cp -rf docs docbuild/content && \
+    mv docbuild/content/assets docbuild/static/content && \
+    cd docbuild && \
+    hugo "$@"
+
