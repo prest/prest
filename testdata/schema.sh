cfe41ac05390efdcf182c373d603493e7e306c4d
Avelino <avelinorun@gmail.com>
2021-06-23 06:34:43 -0300

    create database loadtest
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- testdata/schema.sh
+++ testdata/schema.sh
@@ -4,7 +4,13 @@ DB_USER=${PREST_PG_USER:-postgres}
 DB_PORT=${PREST_PG_PORT:-5432}
 DB_NAME=${PREST_PG_DATABASE:-prest}
 
+# create database var call PREST_PG_DATABASE
 psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "DROP DATABASE IF EXISTS \"$DB_NAME\";"
 psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "create database \"$DB_NAME\";"
 
+# create database loadtest
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "DROP DATABASE IF EXISTS \"loadtest\";"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "create database \"loadtest\";"
+
+# load fixture data
 psql -d $DB_NAME -h $DB_HOST -p $DB_PORT -U $DB_USER -f $GITHUB_WORKSPACE/testdata/schema.sql
