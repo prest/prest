3b322ae2463a414c346895b680c5affefcee1a96
Avelino <avelinorun@gmail.com>
2021-08-28 08:59:22 -0300

    fixer var env on migration test file
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- testdata/migrations_test.sh
+++ testdata/migrations_test.sh
@@ -4,6 +4,6 @@ DB_HOST=${PREST_PG_HOST:-localhost}
 DB_USER=${PREST_PG_USER:-postgres}
 DB_PORT=${PREST_PG_PORT:-5438}
 DB_NAME=${PREST_PG_DATABASE:-prest}
-DB_URL="postgres://${DB_USER}:${PREST_PG_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable"
+DB_URL="postgres://${PREST_PG_USER}:${PREST_PG_PASS}@${PREST_PG_HOST}:${PREST_PG_PORT}/${PREST_PG_DATABASE}?sslmode=disable"
 env go run ./cmd/prestd/main.go migrate --url="$DB_URL" up
 env go run ./cmd/prestd/main.go migrate --url="$DB_URL" up auth
--- testdata/schema.sh
+++ testdata/schema.sh
@@ -3,6 +3,8 @@ DB_HOST=${PREST_PG_HOST:-localhost}
 DB_USER=${PREST_PG_USER:-postgres}
 DB_PORT=${PREST_PG_PORT:-5432}
 DB_NAME=${PREST_PG_DATABASE:-prest}
+GITHUB_WORKSPACE=${GITHUB_WORKSPACE:-"./"}
+export PGPASSWORD=${PREST_PG_PASS:-prest}
 
 # create database var call PREST_PG_DATABASE
 psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "DROP DATABASE IF EXISTS \"$DB_NAME\";"
@@ -12,6 +14,11 @@ psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "create database \"$DB_NAME\";"
 psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "DROP DATABASE IF EXISTS \"loadtest\";"
 psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "create database \"loadtest\";"
 
+# create database prest-test
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "DROP DATABASE IF EXISTS \"prest-test\";"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "create database \"prest-test\";"
+
 # load fixture data
 psql -d $DB_NAME -h $DB_HOST -p $DB_PORT -U $DB_USER -f $GITHUB_WORKSPACE/testdata/schema.sql
 psql -d loadtest -h $DB_HOST -p $DB_PORT -U $DB_USER -f $GITHUB_WORKSPACE/testdata/schema.sql
+psql -d prest-test -h $DB_HOST -p $DB_PORT -U $DB_USER -f $GITHUB_WORKSPACE/testdata/schema.sql
