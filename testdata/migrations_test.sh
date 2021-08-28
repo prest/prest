806585f6f60a5410c068a8c688d45448b9ad1c80
Avelino <avelinorun@gmail.com>
2021-08-28 09:44:50 -0300

    use one database
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- testdata/migrations_test.sh
+++ testdata/migrations_test.sh
@@ -3,7 +3,7 @@ go install
 DB_HOST=${PREST_PG_HOST:-localhost}
 DB_USER=${PREST_PG_USER:-postgres}
 DB_PORT=${PREST_PG_PORT:-5438}
-DB_NAME=${PREST_PG_DATABASE:-prest}
+DB_NAME=${PREST_PG_DATABASE:-prest-test}
 DB_URL="postgres://${PREST_PG_USER}:${PREST_PG_PASS}@${PREST_PG_HOST}:${PREST_PG_PORT}/${PREST_PG_DATABASE}?sslmode=disable"
 env go run ./cmd/prestd/main.go migrate --url="$DB_URL" up
 env go run ./cmd/prestd/main.go migrate --url="$DB_URL" up auth
--- testdata/schema.sh
+++ testdata/schema.sh
@@ -2,23 +2,11 @@
 DB_HOST=${PREST_PG_HOST:-localhost}
 DB_USER=${PREST_PG_USER:-postgres}
 DB_PORT=${PREST_PG_PORT:-5432}
-DB_NAME=${PREST_PG_DATABASE:-prest}
-GITHUB_WORKSPACE=${GITHUB_WORKSPACE:-"./"}
-export PGPASSWORD=${PREST_PG_PASS:-prest}
+DB_NAME=${PREST_PG_DATABASE:-prest-test}
 
 # create database var call PREST_PG_DATABASE
 psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "DROP DATABASE IF EXISTS \"$DB_NAME\";"
-psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "create database \"$DB_NAME\";"
-
-# create database loadtest
-psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "DROP DATABASE IF EXISTS \"loadtest\";"
-psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "create database \"loadtest\";"
-
-# create database prest-test
-psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "DROP DATABASE IF EXISTS \"prest-test\";"
-psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "create database \"prest-test\";"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "CREATE DATABASE \"$DB_NAME\";"
 
 # load fixture data
-psql -d $DB_NAME -h $DB_HOST -p $DB_PORT -U $DB_USER -f $GITHUB_WORKSPACE/testdata/schema.sql
-psql -d loadtest -h $DB_HOST -p $DB_PORT -U $DB_USER -f $GITHUB_WORKSPACE/testdata/schema.sql
-psql -d prest-test -h $DB_HOST -p $DB_PORT -U $DB_USER -f $GITHUB_WORKSPACE/testdata/schema.sql
+psql -d $DB_NAME -h $DB_HOST -p $DB_PORT -U $DB_USER -f ./testdata/schema.sql
