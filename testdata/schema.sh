494c31e81cc295d3333af5eadd14916db89bdf97
Avelino <avelinorun@gmail.com>
2021-08-28 10:06:05 -0300

    database load on ci.yml
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- testdata/schema.sh
+++ testdata/schema.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-PGPASSWORD=${PREST_PG_PASS:-prest} # psql uses another variable name for password
+PGPASSWORD=${PREST_PG_PASS:-postgres} # psql uses another variable name for password
 
 # create database var call PREST_PG_DATABASE
 psql -h $PREST_PG_HOST -p $PREST_PG_PORT -U $PREST_PG_USER -c "DROP DATABASE IF EXISTS \"$PREST_PG_DATABASE\";"
