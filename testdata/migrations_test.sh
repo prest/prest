3c128ea66200ac61bd825eb616b9cd9db9d445ca
Avelino <avelinorun@gmail.com>
2021-08-28 09:57:10 -0300

    clear envvars doent's used
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- testdata/migrations_test.sh
+++ testdata/migrations_test.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 go install
-DB_HOST=${PREST_PG_HOST:-localhost}
-DB_USER=${PREST_PG_USER:-postgres}
-DB_PORT=${PREST_PG_PORT:-5438}
-DB_NAME=${PREST_PG_DATABASE:-prest-test}
-DB_URL="postgres://${PREST_PG_USER}:${PREST_PG_PASS}@${PREST_PG_HOST}:${PREST_PG_PORT}/${PREST_PG_DATABASE}?sslmode=disable"
+DB_URL="postgres://${PREST_PG_USER}:${PREST_PG_PASS}@${PREST_PG_HOST}:${PREST_PG_PORT}/${PREST_PG_DATABASE}?sslmode=${PREST_SSL_MODE}"
 env go run ./cmd/prestd/main.go migrate --url="$DB_URL" up
 env go run ./cmd/prestd/main.go migrate --url="$DB_URL" up auth
--- testdata/schema.sh
+++ testdata/schema.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
-DB_HOST=${PREST_PG_HOST:-localhost}
-DB_USER=${PREST_PG_USER:-postgres}
-DB_PORT=${PREST_PG_PORT:-5432}
-DB_NAME=${PREST_PG_DATABASE:-prest-test}
+PGPASSWORD=${PREST_PG_PASS:-prest} # psql uses another variable name for password
 
 # create database var call PREST_PG_DATABASE
-psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "DROP DATABASE IF EXISTS \"$DB_NAME\";"
-psql -h $DB_HOST -p $DB_PORT -U $DB_USER -c "CREATE DATABASE \"$DB_NAME\";"
+psql -h $PREST_PG_HOST -p $PREST_PG_PORT -U $PREST_PG_USER -c "DROP DATABASE IF EXISTS \"$PREST_PG_DATABASE\";"
+psql -h $PREST_PG_HOST -p $PREST_PG_PORT -U $PREST_PG_USER -c "CREATE DATABASE \"$PREST_PG_DATABASE\";"
 
 # load fixture data
-psql -d $DB_NAME -h $DB_HOST -p $DB_PORT -U $DB_USER -f ./testdata/schema.sql
+psql -h $PREST_PG_HOST -p $PREST_PG_PORT -U $PREST_PG_USER -d $PREST_PG_DATABASE -f ./testdata/schema.sql
