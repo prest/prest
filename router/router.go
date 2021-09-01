ca388bb3260eba330d1e34bf4e66028249e2093e
Avelino <avelinorun@gmail.com>
2021-09-01 14:55:10 -0300

    write test to router package
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- router/router.go
+++ router/router.go
@@ -14,39 +14,40 @@ func initRouter() {
 	router = mux.NewRouter().StrictSlash(true)
 }
 
-// GetRouter for pREST
+// GetRouter reagister all routes
 func GetRouter() *mux.Router {
 	if router == nil {
 		initRouter()
 	}
-	return router
-}
 
-// Routes reagister all routes
-func Routes() *negroni.Negroni {
-	n := middlewares.GetApp()
-	r := GetRouter()
 	// if auth is enabled
 	if config.PrestConf.AuthEnabled {
-		r.HandleFunc("/auth", controllers.Auth).Methods("POST")
+		router.HandleFunc("/auth", controllers.Auth).Methods("POST")
 	}
-	r.HandleFunc("/databases", controllers.GetDatabases).Methods("GET")
-	r.HandleFunc("/schemas", controllers.GetSchemas).Methods("GET")
-	r.HandleFunc("/tables", controllers.GetTables).Methods("GET")
-	r.HandleFunc("/_QUERIES/{queriesLocation}/{script}", controllers.ExecuteFromScripts)
-	r.HandleFunc("/{database}/{schema}", controllers.GetTablesByDatabaseAndSchema).Methods("GET")
-	r.HandleFunc("/show/{database}/{schema}/{table}", controllers.ShowTable).Methods("GET")
+	router.HandleFunc("/databases", controllers.GetDatabases).Methods("GET")
+	router.HandleFunc("/schemas", controllers.GetSchemas).Methods("GET")
+	router.HandleFunc("/tables", controllers.GetTables).Methods("GET")
+	router.HandleFunc("/_QUERIES/{queriesLocation}/{script}", controllers.ExecuteFromScripts)
+	router.HandleFunc("/{database}/{schema}", controllers.GetTablesByDatabaseAndSchema).Methods("GET")
+	router.HandleFunc("/show/{database}/{schema}/{table}", controllers.ShowTable).Methods("GET")
 	crudRoutes := mux.NewRouter().PathPrefix("/").Subrouter().StrictSlash(true)
 	crudRoutes.HandleFunc("/{database}/{schema}/{table}", controllers.SelectFromTables).Methods("GET")
 	crudRoutes.HandleFunc("/{database}/{schema}/{table}", controllers.InsertInTables).Methods("POST")
 	crudRoutes.HandleFunc("/batch/{database}/{schema}/{table}", controllers.BatchInsertInTables).Methods("POST")
 	crudRoutes.HandleFunc("/{database}/{schema}/{table}", controllers.DeleteFromTable).Methods("DELETE")
 	crudRoutes.HandleFunc("/{database}/{schema}/{table}", controllers.UpdateTable).Methods("PUT", "PATCH")
-	r.PathPrefix("/").Handler(negroni.New(
+	router.PathPrefix("/").Handler(negroni.New(
 		middlewares.AccessControl(),
 		middlewares.AuthMiddleware(),
 		negroni.Wrap(crudRoutes),
 	))
-	n.UseHandler(r)
+
+	return router
+}
+
+// Routes for pREST
+func Routes() *negroni.Negroni {
+	n := middlewares.GetApp()
+	n.UseHandler(GetRouter())
 	return n
 }
