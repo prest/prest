4101bf6a3fc9d461d1fb15847e0a9eff25be6a12
Avelino <avelinorun@gmail.com>
2021-09-01 14:55:10 -0300

    write test to router package
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- router/routers_test.go
+++ router/routers_test.go
@@ -1,6 +1,20 @@
 package router
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/prest/prest/adapters/postgres"
+	"github.com/prest/prest/config"
+	"github.com/prest/prest/testutils"
+)
+
+func init() {
+	// load configuration at the beginning of the tests
+	config.Load()
+	postgres.Load()
+}
 
 func TestInitRouter(t *testing.T) {
 	initRouter()
@@ -9,10 +23,38 @@ func TestInitRouter(t *testing.T) {
 	}
 }
 
-func TestGetRouter(t *testing.T) {
+func TestRoutes(t *testing.T) {
 	router = nil
-	r := GetRouter()
+	r := Routes()
 	if r == nil {
 		t.Errorf("Should return a router.")
+	}
+}
+
+func TestDefaultRouters(t *testing.T) {
+	server := httptest.NewServer(GetRouter())
+	defer server.Close()
+
+	var testCases = []struct {
+		url    string
+		method string
+		status int
+	}{
+		{"/databases", "GET", http.StatusOK},
+		{"/schemas", "GET", http.StatusOK},
+		{"/_QUERIES/{queriesLocation}/{script}", "GET", http.StatusBadRequest},
+		{"/{database}/{schema}", "GET", http.StatusBadRequest},
+		{"/show/{database}/{schema}/{table}", "GET", http.StatusBadRequest},
+		{"/{database}/{schema}/{table}", "GET", http.StatusUnauthorized},
+		{"/{database}/{schema}/{table}", "POST", http.StatusUnauthorized},
+		{"/batch/{database}/{schema}/{table}", "POST", http.StatusBadRequest},
+		{"/{database}/{schema}/{table}", "DELETE", http.StatusUnauthorized},
+		{"/{database}/{schema}/{table}", "PUT", http.StatusUnauthorized},
+		{"/{database}/{schema}/{table}", "PATCH", http.StatusUnauthorized},
+		{"/", "GET", http.StatusNotFound},
+	}
+	for _, tc := range testCases {
+		t.Log(tc.method, "\t", tc.url)
+		testutils.DoRequest(t, server.URL+tc.url, nil, tc.method, tc.status, tc.url)
 	}
 }
