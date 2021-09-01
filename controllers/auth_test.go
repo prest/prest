d7afde9aaf37877aee2ae59b6bd52d86eb5681e2
Avelino <avelinorun@gmail.com>
2021-09-01 09:33:45 -0300

    TestAuth Enable and Disable there is a rule that the auth endpoint exists when it is connected to the auth configuration, when it is not it does not exist
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- controllers/auth_test.go
+++ controllers/auth_test.go
@@ -4,6 +4,9 @@ import (
 	"crypto/md5"
 	"crypto/sha1"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/prest/prest/adapters/postgres"
@@ -49,5 +52,37 @@ func Test_encrypt(t *testing.T) {
 	sha1Enc := fmt.Sprintf("%x", sha1.Sum([]byte(pwd)))
 	if enc != sha1Enc {
 		t.Errorf("expected encrypted password to be: %s, got: %s", enc, sha1Enc)
+	}
+}
+
+func TestAuthDisable(t *testing.T) {
+	server := httptest.NewServer(Routes())
+	defer server.Close()
+
+	t.Log("/auth request POST method, disable auth")
+	doRequest(t, server.URL+"/auth", nil, "POST", http.StatusNotFound, "AuthDisable")
+}
+
+func TestAuthEnable(t *testing.T) {
+	os.Setenv("PREST_AUTH_ENABLED", "true")
+	config.Load()
+	postgres.Load()
+
+	server := httptest.NewServer(Routes())
+	defer server.Close()
+
+	var testCases = []struct {
+		description string
+		url         string
+		method      string
+		status      int
+	}{
+		{"/auth request GET method", "/auth", "GET", http.StatusNotFound},
+		{"/auth request POST method", "/auth", "POST", http.StatusUnauthorized},
+	}
+
+	for _, tc := range testCases {
+		t.Log(tc.description)
+		doRequest(t, server.URL+tc.url, nil, tc.method, tc.status, "AuthEnable")
 	}
 }
