330f88cfcc0f56528be6a7b43cd0e89b9be93f00
Avelino <avelinorun@gmail.com>
2021-12-03 13:54:42 -0300

    entrypoint support on env DATABASE_URL fixed: #517
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- etc/entrypoint.sh
+++ etc/entrypoint.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+
+if [ "$DATABASE_URL" ];
+then
+    ## Parse DATABASE URL
+    PROTO="$(echo $DATABASE_URL | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+    # remove the protocol
+    URL="$(echo ${DATABASE_URL/$PROTO/})"
+    # extract the user (if any)
+    USER="$(echo $URL | grep @ | cut -d@ -f1)"
+    # extract the host and port
+    HOSTPORT="$(echo ${URL/$USER@/} | cut -d/ -f1)"
+    # by request host without port
+    PREST_PG_HOST="$(echo $HOSTPORT | sed -e 's,:.*,,g')"
+    # by request - try to extract the port
+    PREST_PG_PORT="$(echo $HOSTPORT | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')"
+    echo "host: $PREST_PG_HOST | port: $PREST_PG_PORT"
+fi
+
 echo "[prestd] Waiting for port $PREST_PG_HOST:$PREST_PG_PORT to become available..."
 while "! nc -z $PREST_PG_HOST $PREST_PG_PORT" 2>/dev/null
 do
