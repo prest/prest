6c0bdfd24da6fdaa182aa34ea2d244d3d2598aa8
Avelino <avelinorun@gmail.com>
2021-12-03 19:35:02 -0300

    use docker release via goreleaser
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- scripts/releaser-tag.sh
+++ scripts/releaser-tag.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
-docker build . -t prest/prest:v1 && \
-docker login -u="$DOCKER_LOGIN" -p="$DOCKER_PASSWORD" && \
-docker push prest/prest:v1 && \
-docker tag prest/prest:v1 prest/prest:$TRAVIS_TAG && \
-docker push prest/prest:$TRAVIS_TAG && \
-git checkout . && \
+if [ -n "$DOCKER_LOGIN" ] && [ -n "$DOCKER_PASSWORD" ]; then
+	echo "Login to the docker..."
+	echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_LOGIN" --password-stdin
+fi
+
+# Workaround for github actions when access to different repositories is needed.
+# Github actions provides a GITHUB_TOKEN secret that can only access the current
+# repository and you cannot configure it's value.
+# Access to different repositories is needed by brew for example.
+
+if [ -n "$GORELEASER_GITHUB_TOKEN" ] ; then
+	export GITHUB_TOKEN=$GORELEASER_GITHUB_TOKEN
+fi
+
+if [ -n "$GITHUB_TOKEN" ]; then
+	# Log into GitHub package registry
+	echo "$GITHUB_TOKEN" | docker login docker.pkg.github.com -u docker --password-stdin
+	echo "$GITHUB_TOKEN" | docker login ghcr.io -u docker --password-stdin
+fi
+
 curl -sL https://git.io/goreleaser | bash
