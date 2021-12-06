855e0054e0f464151ebaf93ed1609a5ae855b86b
Avelino <avelinorun@gmail.com>
2021-12-06 12:30:49 -0300

    use docker/login-action@v1
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- scripts/releaser-tag.sh
+++ scripts/releaser-tag.sh
@@ -1,20 +1,4 @@
 #!/usr/bin/env bash
-if [ -n "$DOCKER_LOGIN" ] && [ -n "$DOCKER_PASSWORD" ]; then
-	echo "Login to the docker..."
-	echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_LOGIN" --password-stdin
-fi
-
-# Workaround for github actions when access to different repositories is needed.
-# Github actions provides a GITHUB_TOKEN secret that can only access the current
-# repository and you cannot configure it's value.
-# Access to different repositories is needed by brew for example.
-
-if [ -n "$GORELEASER_GITHUB_TOKEN" ] ; then
-	export GITHUB_TOKEN=$GORELEASER_GITHUB_TOKEN
-	echo "$GORELEASER_GITHUB_TOKEN" | docker login docker.pkg.github.com -u prest --password-stdin
-	echo "$GORELEASER_GITHUB_TOKEN" | docker login ghcr.io -u prest --password-stdin
-fi
-
 export DOCKER_TAG=${GITHUB_REF#refs/tags/}
 
 git checkout . && \
