d377cd88f438ca39eb2fc38745d0e13fad06c1fd
Avelino <avelinorun@gmail.com>
2021-12-06 11:58:29 -0300

    organize docker tag/build
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- scripts/releaser-tag.sh
+++ scripts/releaser-tag.sh
@@ -18,16 +18,16 @@ fi
 export DOCKER_TAG=${GITHUB_REF#refs/tags/}
 
 git checkout . && \
-    docker build . -t prest/prest:v1 && \
-    docker tag prest/prest:v1 prest/prest:$DOCKER_TAG && \
-    docker tag prest/prest:v1 prest/prest:latest && \
-    docker tag prest/prest:v1 ghcr.io/prest/prest:v1 && \
-    docker tag prest/prest:v1 ghcr.io/prest/prest:$DOCKER_TAG && \
-    docker tag prest/prest:v1 ghcr.io/prest/prest:latest && \
-    docker push ghcr.io/prest/prest:v1 && \
+    docker build . -t ghcr.io/prest/prest:latest && \
+    docker tag ghcr.io/prest/prest:latest prest/prest:$DOCKER_TAG && \
+    docker tag ghcr.io/prest/prest:latest prest/prest:v1 && \
+    docker tag ghcr.io/prest/prest:latest prest/prest:latest && \
+    docker tag ghcr.io/prest/prest:latest ghcr.io/prest/prest:v1 && \
+    docker tag ghcr.io/prest/prest:latest ghcr.io/prest/prest:$DOCKER_TAG && \
     docker push ghcr.io/prest/prest:latest && \
+    docker push ghcr.io/prest/prest:v1 && \
     docker push ghcr.io/prest/prest:$DOCKER_TAG && \
-    docker push prest/prest:v1 && \
     docker push prest/prest:latest && \
+    docker push prest/prest:v1 && \
     docker push prest/prest:$DOCKER_TAG && \
     curl -sL https://git.io/goreleaser | bash
