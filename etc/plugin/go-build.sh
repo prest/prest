61b52f32b04202ca16e17c6e75a98968c07ad5a7
Avelino <avelinorun@gmail.com>
2022-01-04 10:00:09 -0300

    fixed typo buidl to build log
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- etc/plugin/go-build.sh
+++ etc/plugin/go-build.sh
@@ -4,7 +4,7 @@ for fullpath in ./lib/src/*.go; do
     if [ -f "$fullpath" ]; then
 		filename=${fullpath##*/}
 		filename_outext=${filename%%.*}
-		echo "go buidl: $filename_outext plugin..." && \
+		echo "go build: $filename_outext plugin..." && \
 			go build -o lib/${filename_outext}.so -buildmode=plugin lib/src/${filename_outext}.go;
     fi
 done
