d9fed6683eeae9d90e42c1ad1c08e1cd90310321
Avelino <avelinorun@gmail.com>
2022-01-05 06:36:56 -0300

    release 1.0.14
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- helpers/prest.go
+++ helpers/prest.go
@@ -2,7 +2,7 @@ package helpers
 
 var (
 	// PrestVersionNumber repesemts prest version.
-	PrestVersionNumber = "1.0.13"
+	PrestVersionNumber = "1.0.14"
 	// CommitHash for version
 	CommitHash string
 )
