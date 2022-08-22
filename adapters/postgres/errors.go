3ffe238a93e583d847f69a240595ed8b25140059
Arthur Silva <arxdsilva@gmail.com>
2022-08-22 05:35:33 -0300

    add errors

--- adapters/postgres/errors.go
+++ adapters/postgres/errors.go
@@ -5,4 +5,11 @@ import "errors"
 var (
 	ErrJoinInvalidNumberOfArgs = errors.New("invalid number of arguments in join statement")
 	ErrInvalidIdentifier       = errors.New("invalid identifier")
+	ErrInvalidJoinClause       = errors.New("invalid join clause")
+	ErrMustSelectOneField      = errors.New("you must select at least one field")
+	ErrNoTableName             = errors.New("unable to find table name")
+	ErrInvalidOperator         = errors.New("invalid operator")
+	ErrInvalidGroupFn          = errors.New("invalid group function")
+	// ErrBodyEmpty err throw when body is empty
+	ErrBodyEmpty = errors.New("body is empty")
 )
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/lib/pq v1.10.6
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.4.0
 	github.com/spf13/viper v1.12.0
 	github.com/stretchr/testify v1.7.2
