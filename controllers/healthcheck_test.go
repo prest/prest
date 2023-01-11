7690c3471bb11d5171c9edc3f1bb05d94dca94ef
Vinicius Mesel <bioinfo.vinicius@gmail.com>
2023-01-11 13:13:28 -0300

    Updates gomock to v1.6.0 and healthcheck controller

--- controllers/healthcheck_test.go
+++ controllers/healthcheck_test.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/golang/mock/gomock"
 	"github.com/gorilla/mux"
-	"github.com/prest/prest/mocks"
+	"github.com/prest/prest/controllers/mocks"
 	"github.com/prest/prest/testutils"
 )
 
@@ -22,24 +22,10 @@ func TestHealthStatus(t *testing.T) {
 	testutils.DoRequest(t, server.URL+"/_health", nil, "GET", http.StatusOK, "HealthStatus")
 }
 
-func TestMockedHealthcheckFailedConnection(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-	mockedConn := mocks.NewMockDbConnection(mockCtrl)
-	mockedConn.EXPECT().GetConnection().Return(nil, fmt.Errorf("Mocked Connection Failed"))
-	router := mux.NewRouter()
-	router.HandleFunc("/_health", WrappedHealthCheck(mockedConn)).Methods("GET")
-	server := httptest.NewServer(router)
-	defer server.Close()
-
-	testutils.DoRequest(t, server.URL+"/_health", nil, "GET", http.StatusServiceUnavailable, "HealthStatus")
-}
-
 func TestMockedHealthcheckFailedQuery(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 	mockedConn := mocks.NewMockDbConnection(mockCtrl)
-	mockedConn.EXPECT().GetConnection().Return(nil, nil)
 	mockedConn.EXPECT().RunTestQuery().Return(fmt.Errorf("Failed querying"))
 	router := mux.NewRouter()
 	router.HandleFunc("/_health", WrappedHealthCheck(mockedConn)).Methods("GET")
