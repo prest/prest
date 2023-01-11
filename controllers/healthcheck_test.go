14d9030388859354270a2c1cd487ab8ff38a1a7f
Vinicius Mesel <bioinfo.vinicius@gmail.com>
2023-01-11 15:33:43 -0300

    Fixes test with RunTestQuery function renamed to ConnectionTest

--- controllers/healthcheck_test.go
+++ controllers/healthcheck_test.go
@@ -26,7 +26,7 @@ func TestMockedHealthcheckFailedQuery(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 	mockedConn := mocks.NewMockDbConnection(mockCtrl)
-	mockedConn.EXPECT().RunTestQuery().Return(fmt.Errorf("Failed querying"))
+	mockedConn.EXPECT().ConnectionTest().Return(fmt.Errorf("Failed querying"))
 	router := mux.NewRouter()
 	router.HandleFunc("/_health", WrappedHealthCheck(mockedConn)).Methods("GET")
 	server := httptest.NewServer(router)
