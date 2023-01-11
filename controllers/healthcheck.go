9b9d6de66450b5a791620c1bd7370654a7613eb3
Eduardo Cuducos <cuducos@users.noreply.github.com>
2023-01-11 14:57:47 -0500

    Simplifies health endpoint

--- controllers/healthcheck.go
+++ controllers/healthcheck.go
@@ -1,15 +1,10 @@
 package controllers
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/prest/prest/adapters/postgres"
 )
-
-type HealthCheck struct {
-	Status string `json:"status"`
-}
 
 func CheckDBHealth() error {
 	conn, err := postgres.Get()
@@ -26,17 +21,10 @@ func CheckDBHealth() error {
 
 func WrappedHealthCheck(checkDBhealth func() error) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-
 		if err := checkDBhealth(); err != nil {
-			http.Error(w, "unable to run queries on the database", http.StatusServiceUnavailable)
+			w.WriteHeader(http.StatusServiceUnavailable)
 			return
 		}
-
 		w.WriteHeader(http.StatusOK)
-		if err := json.NewEncoder(w).Encode(HealthCheck{"ok"}); err != nil {
-			http.Error(w, "unable to enconde json response", http.StatusServiceUnavailable)
-			return
-		}
 	}
 }
