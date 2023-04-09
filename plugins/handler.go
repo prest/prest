06d3fd270903c5d6ca869fd4ec24d1f68a6bbf0e
Avelino <avelinorun@gmail.com>
2023-04-09 15:50:29 -0300

    plugin: rename handler to single file
    Signed-off-by: Avelino <avelinorun@gmail.com>

---
+++ plugins/handler.go
@@ -0,0 +1,112 @@
+package plugins
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"path/filepath"
+	"plugin"
+
+	"github.com/gorilla/mux"
+	"github.com/prest/prest/cache"
+	"github.com/prest/prest/config"
+)
+
+// LoadedPlugin structure for controlling the loaded plugin
+type LoadedPlugin struct {
+	Loaded bool
+	Plugin *plugin.Plugin
+}
+
+// PluginFuncReturn structure for holding return value and status of plugin function.
+type PluginFuncReturn struct {
+	ReturnJson string
+	StatusCode int
+}
+
+// loadedFunc global variable to control plugins loaded, blocking duplicate loading
+var loadedFunc = map[string]LoadedPlugin{}
+
+// loadFunc private func to load and exec OS Library
+func loadFunc(fileName, funcName string, r *http.Request) (ret PluginFuncReturn, err error) {
+	libPath := filepath.Join(config.PrestConf.PluginPath, fmt.Sprintf("%s.so", fileName))
+	loadedPlugin := loadedFunc[libPath]
+	p := loadedPlugin.Plugin
+	// plugin will be loaded only on the first call to the endpoint
+	if !loadedPlugin.Loaded {
+		p, err = plugin.Open(libPath)
+		if err != nil {
+			return
+		}
+		loadedPlugin = LoadedPlugin{
+			Loaded: true,
+			Plugin: p,
+		}
+		loadedFunc[libPath] = loadedPlugin
+	}
+
+	// HTTPVars populate
+	vars := mux.Vars(r)
+	httpVars, err := p.Lookup("HTTPVars")
+	if err != nil {
+		return
+	}
+	*httpVars.(*map[string]string) = vars
+
+	// URL Query populate
+	urlQuery, err := p.Lookup("URLQuery")
+	if err != nil {
+		return
+	}
+	*urlQuery.(*map[string][]string) = r.URL.Query()
+
+	// function name: HttpMethod+FunctionName+"Handler" (string sufix)
+	// standardizing the name of the method that will be invoked we use
+	// the name Handler as a suffix to identify what will be called in the http
+	f, err := p.Lookup(fmt.Sprintf("%s%sHandler", r.Method, funcName))
+	if err != nil {
+		return
+	}
+	// Exec (call) function name, return string (In case which return status code does not matter)
+	function, ok := f.(func() string)
+
+	if !ok {
+		// It is probable that plugin function return not only json but also status code.
+		function := f.(func() (string, int))
+		retJson, code := function()
+		ret.ReturnJson = retJson
+		ret.StatusCode = code
+
+		log.Printf("ret plugin(status %d): %s\n", code, ret.ReturnJson)
+	} else {
+		retJson := function()
+		ret.ReturnJson = retJson
+		ret.StatusCode = -1
+
+		log.Println("ret plugin:", ret.ReturnJson)
+	}
+
+	return
+}
+
+// HandlerPlugin responsible for processing the `.so` function via http protocol
+func HandlerPlugin(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fileName := vars["file"]
+	funcName := vars["func"]
+	ret, err := loadFunc(fileName, funcName, r)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	// Cache arrow if enabled
+	cache.BuntSet(r.URL.String(), ret.ReturnJson)
+
+	//nolint
+	if ret.StatusCode != -1 {
+		w.WriteHeader(ret.StatusCode)
+	}
+
+	w.Write([]byte(ret.ReturnJson))
+}
---
+++ plugins/handler_test.go
@@ -0,0 +1,48 @@
+package plugins
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/prest/prest/adapters/postgres"
+	"github.com/prest/prest/config"
+	"github.com/prest/prest/testutils"
+)
+
+func initAuthRoutes() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/_PLUGIN/{file}/{func}", HandlerPlugin)
+	return r
+}
+
+func TestPlugins(t *testing.T) {
+	config.Load()
+	postgres.Load()
+	// running the tests at this point the working folder will be the plugins
+	// package folder, so to return a directory `../`
+	config.PrestConf.PluginPath = "../lib"
+	server := httptest.NewServer(initAuthRoutes())
+	defer server.Close()
+
+	var testCases = []struct {
+		description string
+		url         string
+		method      string
+		status      int
+	}{
+		// TODO: should be status 200 `http.StatusOK`, but read
+		{"/_PLUGIN/hello/Hello request GET method", "/_PLUGIN/hello/Hello", "GET", http.StatusNotFound},
+		{"/_PLUGIN/hello/HelloWithStatus request GET method", "/_PLUGIN/hello/HelloWithStatus", "GET", http.StatusNotFound},
+		{"/_PLUGIN/hello/Hello request POST method", "/_PLUGIN/hello/Hello", "POST", http.StatusNotFound},
+	}
+
+	// TODO: tests will not work because the plugin system has an error loading at runtime
+	// plugin.Open("../lib/hello"): plugin was built with a different version of package runtime
+	// ref: https://github.com/golang/go/issues/27751
+	for _, tc := range testCases {
+		t.Log(tc.description)
+		testutils.DoRequest(t, server.URL+tc.url, nil, tc.method, tc.status, "Plugins")
+	}
+}
