615dd1790cd1f200ad56e0cfec5bd38d4a43bb39
Felipe Oliveira <fpo@felipeweb.dev>
2023-04-27 17:48:54 -0300

    add test
    Signed-off-by: Felipe Oliveira <fpo@felipeweb.dev>

--- lib/src/middlewares/hello.go
+++ lib/src/middlewares/hello.go
@@ -10,9 +10,10 @@ import (
 )
 
 // BUILD:
-// go build -o ./lib/hello_midlleware.so -buildmode=plugin ./lib/src/middlewares/hello.go
+// go build -o ./lib/midllewares/hello.so -buildmode=plugin ./lib/src/middlewares/hello.go
 func HelloMiddlewareLoad() negroni.Handler {
 	return negroni.HandlerFunc(func(rw http.ResponseWriter, rq *http.Request, next http.HandlerFunc) {
+		rw.Header().Add("X-Hello-Middleware", "Hello Middleware")
 		next(rw, rq)
 	})
 }
