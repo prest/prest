f48546a78b5bd74657283ebab86c92b195e7c243
Arthur Silva <arxdsilva@gmail.com>
2023-12-24 00:38:30 -0800

    add Test_Route_Databases

--- router/router_test.go
+++ router/router_test.go
@@ -19,100 +19,166 @@ func TestRoutes(t *testing.T) {
 }
 
 // todo: split this into mini tests
-func TestDefaultRouters(t *testing.T) {
-	// server := httptest.NewServer(New(&config.Prest{}).router)
-	// defer server.Close()
+func Test_DefaultRouters(t *testing.T) {
 
-	// var testCases = []struct {
-	// 	url    string
-	// 	method string
-	// 	status int
+	var testCases = []struct {
+		url    string
+		method string
+		status int
 
-	// 	wantGetDBs bool
-	// }{
-	// 	{
-	// 		url:        "/databases",
-	// 		method:     "GET",
-	// 		status:     http.StatusOK,
-	// 		wantGetDBs: true,
-	// 	},
-	// {
-	// 	url:        "/schemas",
-	// 	method:     "GET",
-	// 	status:     http.StatusOK,
-	// 	wantGetDBs: false,
-	// },
-	// {
-	// 	url:        "/_QUERIES/{queriesLocation}/{script}",
-	// 	method:     "GET",
-	// 	status:     http.StatusBadRequest,
-	// 	wantGetDBs: false,
-	// },
-	// {
-	// 	url:        "/{database}/{schema}",
-	// 	method:     "GET",
-	// 	status:     http.StatusBadRequest,
-	// 	wantGetDBs: false,
-	// },
-	// {
-	// 	url:        "/show/{database}/{schema}/{table}",
-	// 	method:     "GET",
-	// 	status:     http.StatusBadRequest,
-	// 	wantGetDBs: false,
-	// },
-	// {
-	// 	url:        "/{database}/{schema}/{table}",
-	// 	method:     "GET",
-	// 	status:     http.StatusUnauthorized,
-	// 	wantGetDBs: false,
-	// },
-	// {
-	// 	url:        "/{database}/{schema}/{table}",
-	// 	method:     "POST",
-	// 	status:     http.StatusUnauthorized,
-	// 	wantGetDBs: false,
-	// },
-	// {
-	// 	url:        "/batch/{database}/{schema}/{table}",
-	// 	method:     "POST",
-	// 	status:     http.StatusBadRequest,
-	// 	wantGetDBs: false,
-	// },
-	// {
-	// 	url:        "/{database}/{schema}/{table}",
-	// 	method:     "DELETE",
-	// 	status:     http.StatusUnauthorized,
-	// 	wantGetDBs: false,
-	// },
-	// {
-	// 	url:        "/{database}/{schema}/{table}",
-	// 	method:     "PUT",
-	// 	status:     http.StatusUnauthorized,
-	// 	wantGetDBs: false,
-	// },
-	// {
-	// 	url:        "/{database}/{schema}/{table}",
-	// 	method:     "PATCH",
-	// 	status:     http.StatusUnauthorized,
-	// 	wantGetDBs: false,
-	// },
-	// {
-	// 	url:        "/auth",
-	// 	method:     "GET",
-	// 	status:     http.StatusNotFound,
-	// 	wantGetDBs: false,
-	// },
-	// {
-	// 	url:        "/",
-	// 	method:     "GET",
-	// 	status:     http.StatusNotFound,
-	// 	wantGetDBs: false,
-	// },
-	// }
-	// for _, tc := range testCases {
-	// 	t.Log(tc.method, "\t", tc.url)
-	// 	testutils.DoRequest(t, server.URL+tc.url, nil, tc.method, tc.status, tc.url)
-	// }
+		wantGetDBs bool
+	}{
+		{
+			url:        "/databases",
+			method:     "GET",
+			status:     http.StatusOK,
+			wantGetDBs: true,
+		},
+		// {
+		// 	url:        "/schemas",
+		// 	method:     "GET",
+		// 	status:     http.StatusOK,
+		// 	wantGetDBs: false,
+		// },
+		// {
+		// 	url:        "/_QUERIES/{queriesLocation}/{script}",
+		// 	method:     "GET",
+		// 	status:     http.StatusBadRequest,
+		// 	wantGetDBs: false,
+		// },
+		// {
+		// 	url:        "/{database}/{schema}",
+		// 	method:     "GET",
+		// 	status:     http.StatusBadRequest,
+		// 	wantGetDBs: false,
+		// },
+		// {
+		// 	url:        "/show/{database}/{schema}/{table}",
+		// 	method:     "GET",
+		// 	status:     http.StatusBadRequest,
+		// 	wantGetDBs: false,
+		// },
+		// {
+		// 	url:        "/{database}/{schema}/{table}",
+		// 	method:     "GET",
+		// 	status:     http.StatusUnauthorized,
+		// 	wantGetDBs: false,
+		// },
+		// {
+		// 	url:        "/{database}/{schema}/{table}",
+		// 	method:     "POST",
+		// 	status:     http.StatusUnauthorized,
+		// 	wantGetDBs: false,
+		// },
+		// {
+		// 	url:        "/batch/{database}/{schema}/{table}",
+		// 	method:     "POST",
+		// 	status:     http.StatusBadRequest,
+		// 	wantGetDBs: false,
+		// },
+		// {
+		// 	url:        "/{database}/{schema}/{table}",
+		// 	method:     "DELETE",
+		// 	status:     http.StatusUnauthorized,
+		// 	wantGetDBs: false,
+		// },
+		// {
+		// 	url:        "/{database}/{schema}/{table}",
+		// 	method:     "PUT",
+		// 	status:     http.StatusUnauthorized,
+		// 	wantGetDBs: false,
+		// },
+		// {
+		// 	url:        "/{database}/{schema}/{table}",
+		// 	method:     "PATCH",
+		// 	status:     http.StatusUnauthorized,
+		// 	wantGetDBs: false,
+		// },
+		// {
+		// 	url:        "/auth",
+		// 	method:     "GET",
+		// 	status:     http.StatusNotFound,
+		// 	wantGetDBs: false,
+		// },
+		// {
+		// 	url:        "/",
+		// 	method:     "GET",
+		// 	status:     http.StatusNotFound,
+		// 	wantGetDBs: false,
+		// },
+	}
+	for _, tc := range testCases {
+		ctrl := gomock.NewController(t)
+		adapter := mockgen.NewMockAdapter(ctrl)
+
+		ctrl2 := gomock.NewController(t)
+		adapter2 := mockgen.NewMockScanner(ctrl2)
+
+		adapter.EXPECT().Query(" ").Return(adapter2)
+
+		adapter2.EXPECT().Err().Return(nil)
+		adapter2.EXPECT().Bytes().Return([]byte("[]"))
+
+		adapter.EXPECT().WhereByRequest(gomock.Any(), gomock.Any()).Return("", nil, nil)
+		adapter.EXPECT().DatabaseWhere(gomock.Any()).Return("")
+		adapter.EXPECT().DatabaseClause(gomock.Any()).Return("", false)
+		adapter.EXPECT().DistinctClause(gomock.Any()).Return("", nil)
+		adapter.EXPECT().OrderByRequest(gomock.Any()).Return("", nil)
+		adapter.EXPECT().DatabaseOrderBy(gomock.Any(), gomock.Any()).Return("")
+		adapter.EXPECT().PaginateIfPossible(gomock.Any()).Return("", nil)
+
+		cfg := &config.Prest{
+			Adapter: adapter,
+		}
+		server := httptest.NewServer(New(cfg).router)
+
+		t.Log(tc.method, "\t", tc.url)
+		testutils.DoRequest(t, server.URL+tc.url, nil, tc.method, tc.status, tc.url)
+		server.Close()
+	}
+}
+
+func Test_Route_Databases(t *testing.T) {
+	var testCases = []struct {
+		url    string
+		method string
+		status int
+	}{
+		{
+			url:    "/databases",
+			method: "GET",
+			status: http.StatusOK,
+		},
+	}
+	ctrl := gomock.NewController(t)
+	adapter := mockgen.NewMockAdapter(ctrl)
+
+	ctrl2 := gomock.NewController(t)
+	adapter2 := mockgen.NewMockScanner(ctrl2)
+
+	adapter.EXPECT().Query(" ").Return(adapter2)
+
+	adapter2.EXPECT().Err().Return(nil)
+	adapter2.EXPECT().Bytes().Return([]byte("[]"))
+
+	adapter.EXPECT().WhereByRequest(gomock.Any(), gomock.Any()).Return("", nil, nil)
+	adapter.EXPECT().DatabaseWhere(gomock.Any()).Return("")
+	adapter.EXPECT().DatabaseClause(gomock.Any()).Return("", false)
+	adapter.EXPECT().DistinctClause(gomock.Any()).Return("", nil)
+	adapter.EXPECT().OrderByRequest(gomock.Any()).Return("", nil)
+	adapter.EXPECT().DatabaseOrderBy(gomock.Any(), gomock.Any()).Return("")
+	adapter.EXPECT().PaginateIfPossible(gomock.Any()).Return("", nil)
+
+	cfg := &config.Prest{
+		Adapter: adapter,
+	}
+	server := httptest.NewServer(New(cfg).router)
+
+	for _, tc := range testCases {
+		t.Log(tc.method, "\t", tc.url)
+		testutils.DoRequest(t, server.URL+tc.url, nil, tc.method, tc.status, tc.url)
+		server.Close()
+	}
 }
 
 func Test_AuthRouteActive_NotFound(t *testing.T) {
