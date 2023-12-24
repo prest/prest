2ac129db343b5698ff4fd68262f511a013637edd
Arthur Silva <arxdsilva@gmail.com>
2023-12-24 01:30:39 -0800

    fix timeout default value

--- context/keys.go
+++ context/keys.go
@@ -16,5 +16,8 @@ const (
 
 func WithTimeout(ctx context.Context) (context.Context, context.CancelFunc) {
 	timeout, _ := ctx.Value(HTTPTimeoutKey).(int)
+	if timeout == 0 {
+		timeout = 60
+	}
 	return context.WithTimeout(ctx, time.Second*time.Duration(timeout))
 }
