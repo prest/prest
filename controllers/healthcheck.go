6d56a60683c3286e8b1986f330c6bd5609466f31
Arthur Silva <arxdsilva@gmail.com>
2024-01-15 20:24:05 -0800

    add hc docs

--- controllers/healthcheck.go
+++ controllers/healthcheck.go
@@ -15,6 +15,11 @@ var DefaultCheckList = CheckList{
 	CheckDBHealth,
 }
 
+// CheckDBHealth checks the health of the database by executing a simple query.
+// It takes a context and an adapter as parameters.
+// The adapter is used to establish a connection to the database.
+// If the connection is successful, it executes a query and commits the transaction.
+// If any error occurs during the process, it returns the error.
 func CheckDBHealth(ctx context.Context, adptr adapters.Adapter) error {
 	conn, err := adptr.GetTransactionCtx(ctx)
 	if err != nil {
@@ -27,6 +32,13 @@ func CheckDBHealth(ctx context.Context, adptr adapters.Adapter) error {
 	return conn.Commit()
 }
 
+// WrappedHealthCheck is a handler function that performs health checks on a list of checks.
+// It takes a CheckList as input and returns an http.HandlerFunc.
+//
+// Each check in the CheckList is executed, and if any check fails, the handler
+// responds with a 503 Service Unavailable status code.
+//
+// If all checks pass, the handler responds with a 200 OK status code.
 func (c *Config) WrappedHealthCheck(checks CheckList) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx, cancel := pctx.WithTimeout(r.Context())
