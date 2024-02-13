31bcc570c785bf3862c874529b068a9f4712b4d5
Arthur Silva <arxdsilva@gmail.com>
2024-02-12 17:39:10 -0800

    router: add Test_ConfigRoutes_schemas

--- router/router_test.go
+++ router/router_test.go
@@ -130,3 +130,35 @@ func Test_ConfigRoutes_databases(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, http.StatusOK, resp.StatusCode)
 }
+
+func Test_ConfigRoutes_schemas(t *testing.T) {
+	t.Parallel()
+
+	r := cfg
+
+	ctrl := gomock.NewController(t)
+	srv := srvMock.NewMockServer(ctrl)
+
+	srv.EXPECT().WrappedHealthCheck(gomock.Any()).AnyTimes().Do(
+		func(check interface{}) {})
+
+	ma := adptMock.NewMockAdapter(ctrl)
+
+	srv.EXPECT().GetAdapter().AnyTimes().Return(ma)
+
+	srv.EXPECT().GetSchemas(gomock.Any(), gomock.Any()).AnyTimes().Do(
+		func(w, r interface{}) {})
+
+	err := r.ConfigRoutes(srv)
+	require.NoError(t, err)
+
+	nr := negroni.New()
+	nr.UseHandler(r.router)
+
+	testSrv := httptest.NewServer(nr)
+	defer testSrv.Close()
+
+	resp, err := http.Get(testSrv.URL + "/schemas")
+	require.NoError(t, err)
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+}
