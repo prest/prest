954a8d045afccd5979c54de5fcbcdb6fa3091063
Mathieu Bouchard <mathieu.bouchard@mcn.gouv.qc.ca>
2024-05-27 10:58:29 -0400

    Unify comments

--- scripts/docker-hub-tag.sh
+++ scripts/docker-hub-tag.sh
@@ -2,12 +2,14 @@
 export DOCKER_TAG=${GITHUB_REF#refs/tags/}
 
 git checkout . && \
+    # Build the default image
     docker build . -t prest/prest:latest && \
     docker tag prest/prest:latest prest/prest:"$DOCKER_TAG" && \
     docker tag prest/prest:latest prest/prest:v1 && \
     docker push prest/prest:latest && \
     docker push prest/prest:v1 && \
     docker push prest/prest:"$DOCKER_TAG" && \
+    # Build the noplugins image
     docker build . -f Dockerfile.noplugins -t prest/prest:latest-noplugins && \
     docker tag prest/prest:latest prest/prest:"$DOCKER_TAG"-noplugins && \
     docker tag prest/prest:latest prest/prest:v1-noplugins && \
