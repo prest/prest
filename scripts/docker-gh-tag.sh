ae25673b00bccf069babb9713f94ce92fdcadcd3
Mathieu Bouchard <mathieu.bouchard@mcn.gouv.qc.ca>
2024-05-27 09:55:36 -0400

    Add double quotes around  to prevent globbing and word splitting

--- scripts/docker-gh-tag.sh
+++ scripts/docker-gh-tag.sh
@@ -5,14 +5,14 @@ git checkout . && \
     # Build the default image
     docker build . -t ghcr.io/prest/prest:latest && \
     docker tag ghcr.io/prest/prest:latest ghcr.io/prest/prest:v1 && \
-    docker tag ghcr.io/prest/prest:latest ghcr.io/prest/prest:$DOCKER_TAG && \
+    docker tag ghcr.io/prest/prest:latest ghcr.io/prest/prest:"$DOCKER_TAG" && \
     docker push ghcr.io/prest/prest:latest && \
     docker push ghcr.io/prest/prest:v1 && \
-    docker push ghcr.io/prest/prest:$DOCKER_TAG && \
+    docker push ghcr.io/prest/prest:"$DOCKER_TAG" && \
     # Build the noplugins image
     docker build . -f Dockerfile.noplugins -t ghcr.io/prest/prest:latest-noplugins && \
     docker tag ghcr.io/prest/prest:latest ghcr.io/prest/prest:v1-noplugins && \
-    docker tag ghcr.io/prest/prest:latest ghcr.io/prest/prest:$DOCKER_TAG-noplugins && \
+    docker tag ghcr.io/prest/prest:latest ghcr.io/prest/prest:"$DOCKER_TAG"-noplugins && \
     docker push ghcr.io/prest/prest:latest-noplugins && \
     docker push ghcr.io/prest/prest:v1-noplugins && \
-    docker push ghcr.io/prest/prest:$DOCKER_TAG-noplugins
+    docker push ghcr.io/prest/prest:"$DOCKER_TAG"-noplugins
--- scripts/docker-hub-tag.sh
+++ scripts/docker-hub-tag.sh
@@ -3,14 +3,14 @@ export DOCKER_TAG=${GITHUB_REF#refs/tags/}
 
 git checkout . && \
     docker build . -t prest/prest:latest && \
-    docker tag prest/prest:latest prest/prest:$DOCKER_TAG && \
+    docker tag prest/prest:latest prest/prest:"$DOCKER_TAG" && \
     docker tag prest/prest:latest prest/prest:v1 && \
     docker push prest/prest:latest && \
     docker push prest/prest:v1 && \
-    docker push prest/prest:$DOCKER_TAG && \
+    docker push prest/prest:"$DOCKER_TAG" && \
     docker build . -f Dockerfile.noplugins -t prest/prest:latest-noplugins && \
-    docker tag prest/prest:latest prest/prest:$DOCKER_TAG-noplugins && \
+    docker tag prest/prest:latest prest/prest:"$DOCKER_TAG"-noplugins && \
     docker tag prest/prest:latest prest/prest:v1-noplugins && \
     docker push prest/prest:latest-noplugins && \
     docker push prest/prest:v1-noplugins && \
-    docker push prest/prest:$DOCKER_TAG-noplugins
+    docker push prest/prest:"$DOCKER_TAG"-noplugins
