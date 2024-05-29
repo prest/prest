33208ef1608b5e7b618d76803f14b28d92f08731
Mathieu Bouchard <83231959+bouchardmathieu-qc@users.noreply.github.com>
2024-05-29 18:32:06 -0400

    Create a new image where plugins cannot be built at runtime (#883)

--- scripts/docker-beta.sh
+++ scripts/docker-beta.sh
@@ -4,4 +4,6 @@
 # the beta tag in the github package (docker)
 git checkout . && \
     docker build . -t ghcr.io/prest/prest:beta && \
-    docker push ghcr.io/prest/prest:beta
+    docker push ghcr.io/prest/prest:beta && \
+    docker build . -f Dockerfile.noplugins -t ghcr.io/prest/prest:beta-noplugins && \
+    docker push ghcr.io/prest/prest:beta-noplugins
--- scripts/docker-gh-tag.sh
+++ scripts/docker-gh-tag.sh
@@ -2,9 +2,17 @@
 export DOCKER_TAG=${GITHUB_REF#refs/tags/}
 
 git checkout . && \
+    # Build the default image
     docker build . -t ghcr.io/prest/prest:latest && \
     docker tag ghcr.io/prest/prest:latest ghcr.io/prest/prest:v1 && \
-    docker tag ghcr.io/prest/prest:latest ghcr.io/prest/prest:$DOCKER_TAG && \
+    docker tag ghcr.io/prest/prest:latest ghcr.io/prest/prest:"$DOCKER_TAG" && \
     docker push ghcr.io/prest/prest:latest && \
     docker push ghcr.io/prest/prest:v1 && \
-    docker push ghcr.io/prest/prest:$DOCKER_TAG
+    docker push ghcr.io/prest/prest:"$DOCKER_TAG" && \
+    # Build the noplugins image
+    docker build . -f Dockerfile.noplugins -t ghcr.io/prest/prest:"latest-noplugins" && \
+    docker tag ghcr.io/prest/prest:latest ghcr.io/prest/prest:"v1-noplugins" && \
+    docker tag ghcr.io/prest/prest:latest ghcr.io/prest/prest:"$DOCKER_TAG-noplugins" && \
+    docker push ghcr.io/prest/prest:"latest-noplugins" && \
+    docker push ghcr.io/prest/prest:"v1-noplugins" && \
+    docker push ghcr.io/prest/prest:"$DOCKER_TAG-noplugins"
